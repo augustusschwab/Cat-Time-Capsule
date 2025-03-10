1ee5f135821e030b5ee8ad36d6b058f12116b793
beth-em <beth.murillo318@gmail.com>
2025-03-10 12:50:46 -0600

    css for all files added to index.css and updated some tsx files

--- server/src/seeds/user-seeds.ts
+++
@@ -1,9 +0,0 @@
-import {User} from '../models/index.js';
-
-export const seedUsers = async () => {
-  await User.bulkCreate([
-    { username: 'CatLover', email: 'cat@lover.com', password: 'password' },
-    { username: 'CatsAreCool', email: 'catsare@cool.com', password: 'password' },
-    { username: 'AwesomeCats', email: 'awesome@cats.com', password: 'password' },
-  ], { individualHooks: true });
-};
